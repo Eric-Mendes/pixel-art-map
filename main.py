4465659a87ecfc9648af2433c1ece86a926826c8
Eric Mendes <42689328+Eric-Mendes@users.noreply.github.com>
2021-11-16 21:55:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from math import sqrt
+import pandas as pd
+from PIL import Image
+import numpy as np
+
+def distance(p1, p2):
+    return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2 + (p1[2] - p2[2])**2)
+
+df_blocks = pd.read_json("./blocks.json")
+df_blocks.drop([0], inplace=True)
+
+def decide_color(pxl):
+    df_blocks['distance'] = df_blocks['rgb'].apply(lambda rgb: distance([int(i) for i in rgb], pxl))
+
+    df_blocks.sort_values(by='distance', inplace=True)
+    print(df_blocks.iloc[0])
+    return df_blocks.iloc[0]['blocks']
+
+
+img = Image.open(r'./cbf.png').convert("RGB")
+img = img.resize((img.size[0]//4, img.size[1]//4))
+arr = np.array(img)
+
+result = []
+for row in arr:
+    r = []
+    for col in row:
+        dc = decide_color(col)
+        r.append(dc)
+    result.append(r)
+
+
+pd.DataFrame(result).to_csv(path_or_buf="creeper_head.csv", index=False, header=False)
